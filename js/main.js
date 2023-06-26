8a2d35c4714ad94363a4d7d195c5693de3029f49
Isabela Baseggio <isabela.baseggio7@gmail.com>
2023-06-26 19:02:29 -0300

    setItem nome and quantidade

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log(document.getElementById("novoItem"));
+const form = document.getElementById("novoItem");
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value="";
+    quantidade.value = "";
+})
+
+function criaElemento(nome, quantidade) {
+    
+    //<li class="item"><strong>3</strong>Casaco</li>
+    const novoItem = document.createElement('lis');
+    novoItem.classList.add('item');
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    
+    novoItem.innerHTML += nome;
+
+    const lista = document.getElementById('lista');
+    lista.appendChild(novoItem);
+
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("quantidade", quantidade);
+};
